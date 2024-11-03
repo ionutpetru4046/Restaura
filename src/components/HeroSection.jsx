4babdefcf723371edeed474fedd16ef41ebc7d8a
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-03 20:24:00 +0000

    new changes to hero section added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,14 @@
-
+import video from "../assets/hero.mp4"
+import logo from "../assets/logo.png"
+import hero from "../assets/hero.jpeg"
 
 const HeroSection = () => {
   return (
-    <div>
-      hero section new
-    </div>
+    <section className="relative flex h-screen items-center justify-center">
+      <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
+        <video src={video} className="h-full w-full object-cover" muted autoPlay loop playsInline poster={hero}></video>
+      </div>
+    </section>
   )
 }
 
