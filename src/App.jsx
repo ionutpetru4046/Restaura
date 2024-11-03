d7de5ae77a0dd2336612127e57705ffb9356e4ae
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-03 21:32:37 +0000

    DishCard and Dishes section components finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import DishCard from "./components/DishCard"
+import Dishes from "./components/Dishes"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/NavBar"
 
@@ -7,7 +7,7 @@ const App = () => {
     <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
       <Navbar />
-      <DishCard />
+      <Dishes />
     </main>
   )
 }
--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,8 +1,19 @@
+/* eslint-disable react/prop-types */
 
-const DishCard = () => {
+const DishCard = ({ project }) => {
   return (
     <div>
-        Dishes
+        <img src={project.image} 
+             alt={project.title}
+             className="rounded-3xl p-2" />
+        <div className="p-4">
+            <h3 className="mb-2 text-2xl font-bold tracking-tighter">
+                {project.title}    
+            </h3>
+            <p className="text-sm">
+                {project.description}    
+            </p>    
+        </div>     
     </div>
   )
 }
