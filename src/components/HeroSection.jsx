b59b7cf61bb15ea82d5b3fba46ab2e3f82fd2470
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-03 20:31:59 +0000

    HeroSection finished

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,6 +8,11 @@ const HeroSection = () => {
       <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
         <video src={video} className="h-full w-full object-cover" muted autoPlay loop playsInline poster={hero}></video>
       </div>
+      <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black"></div>
+      <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
+        <img src={logo} alt="restaura" className="w-full p-4" />
+        <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
+      </div>
     </section>
   )
 }
