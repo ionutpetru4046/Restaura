c96a94c20af47015801f096d851df6bf25790bcb
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-03 20:36:46 +0000

    new component added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/HeroSection"
+import NavBar from "./components/NavBar"
 
 const App = () => {
   return (
-    <div>
+    <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
-    </div>
+      <NavBar />
+    </main>
   )
 }
 
