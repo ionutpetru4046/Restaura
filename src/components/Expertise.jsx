e81cdde9479b7a8d08f3dd3536a508b9076b58d6
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-05 19:52:12 +0000

    h2 added to the expertise component

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -1,8 +1,9 @@
 const Expertise = () => {
   return (
-    <div>
-        
-    </div>
+    <section id="expertise">
+        <h2 className="my-8 text-center text-3xl tracking-tighter 
+        lg:text-4xl">Our Expertise</h2>
+    </section>
   )
 }
 
