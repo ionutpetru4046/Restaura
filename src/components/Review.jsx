8b680f7c28c23e22ce80352c7d8db49e34823c45
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-06 20:05:27 +0000

    changes added to review component img added

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,4 +1,9 @@
 import { REVIEW } from "../constants"
+import xaviour from "../assets/xaviour.jpeg"
+import customer1 from "../assets/customer1.jpeg"
+import customer2 from "../assets/customer2.jpeg"
+import customer3 from "../assets/customer3.jpeg"
+import customer4 from "../assets/customer4.jpeg"
 
 const Review = () => {
   return (
@@ -8,6 +13,16 @@ const Review = () => {
             tracking-tighter lg:mx-40 lg:mt-40 lg:text-[3.5rem]">
                 {REVIEW.content}
             </p>
+            <div className="flex items-center justify-center gap-6">
+                <img src={xaviour} width={80} height={80} alt={REVIEW.name} 
+                className="rounded-full border" />
+                <div className="tracking-tighter">
+                    <h6>{REVIEW.name}</h6>
+                    <p className="text-sm text-neutral-500">
+                        {REVIEW.profession}
+                    </p>
+                </div>
+            </div>
         </div>
     </section>
   )
