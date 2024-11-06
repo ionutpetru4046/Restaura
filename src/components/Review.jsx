3c5fe24a25d248b4af713229370ffa0f859544f9
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-06 19:54:20 +0000

    did and paragraph added to the review component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,7 +1,14 @@
+import { REVIEW } from "../constants"
+
 const Review = () => {
   return (
     <section className="container mx-auto mb-8 mt-12" id="review">
-        
+        <div className="flex flex-col">
+            <p className="mb-10 text-3xl font-light leading-normal 
+            tracking-tighter lg:mx-40 lg:mt-40 lg:text-[3.5rem]">
+                {REVIEW.content}
+            </p>
+        </div>
     </section>
   )
 }
