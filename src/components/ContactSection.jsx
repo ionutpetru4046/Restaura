ac910b7317ed88323e5513fda30b210048a630b9
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-07 20:06:32 +0000

    h2 added to the Contact component

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,7 +1,10 @@
 const ContactSection = () => {
   return (
     <section className="container mx-auto py-16" id="contact">
-        
+        <h2 className="mb-8 text-center text-3xl lg:text-4xl">
+          Contact Us
+        </h2>
+        <div className=""></div>
     </section>
   )
 }
