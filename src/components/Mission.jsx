446316b1e9db97b27e21ab9681f1fc4ef8ce4c42
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-08 14:58:23 +0000

    animations added to the mission component video section

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,6 +1,7 @@
 import missionImg from "../assets/mission.jpeg"
 import mission from "../assets/mission.mp4"
 import { MISSION } from "../constants"
+import { motion } from "framer-motion"
 
 const Mission = () => {
   return (
@@ -8,10 +9,15 @@ const Mission = () => {
         <div className="container mx-auto text-center">
           <h2 className="mb-8 text-3xl lg:text-4xl">Out Mission</h2>
           <div className="relative flex items-center justify-center">
-            <video className="w-full rounded-3xl" autoPlay muted loop 
-            playsInline poster={missionImg}>
+            <motion.video className="w-full rounded-3xl" autoPlay muted loop 
+            playsInline 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1}}
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            poster={missionImg}>
               <source src={mission} type="video/mp4" />
-            </video>
+            </motion.video>
             <div className="absolute h-full w-full rounded-3xl bg-black/40">
             </div>
             <p className="absolute max-w-lg tracking-tighter lg:text-3xl">
