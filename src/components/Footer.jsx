077db45fda962efad69b30f383721e710f86ebc4
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-08 14:23:30 +0000

    icons and link added to the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import { SOCIAL_MEDIA_LINKS } from "../constants"
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className="mb-8 mt-20">
+        <div className="flex items-center justify-center gap-8">
+          {SOCIAL_MEDIA_LINKS.map((link, index) => (
+            <a key={index} href={link.href} target="_blank" rel="noopener noreferrer">
+              {link.icon}
+            </a>
+          ))}
+        </div>
     </div>
   )
 }
