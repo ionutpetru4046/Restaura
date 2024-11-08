1e63b8f4add752fc78600f22a905d61715ce726a
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-08 15:08:50 +0000

    last animation added to the mission component

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -9,20 +9,29 @@ const Mission = () => {
         <div className="container mx-auto text-center">
           <h2 className="mb-8 text-3xl lg:text-4xl">Out Mission</h2>
           <div className="relative flex items-center justify-center">
-            <motion.video className="w-full rounded-3xl" autoPlay muted loop 
-            playsInline 
+            <motion.video 
+            className="w-full rounded-3xl" autoPlay muted loop playsInline 
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1}}
             viewport={{ once: true }}
             transition={{ duration: 1 }}
             poster={missionImg}>
-              <source src={mission} type="video/mp4" />
+            <source src={mission} type="video/mp4" />
             </motion.video>
-            <div className="absolute h-full w-full rounded-3xl bg-black/40">
-            </div>
-            <p className="absolute max-w-lg tracking-tighter lg:text-3xl">
+            <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className="absolute h-full w-full rounded-3xl bg-black/40" />
+            <motion.p 
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 1, delay: 0.5 }}
+              className="absolute max-w-lg tracking-tighter lg:text-3xl">
               {MISSION}
-            </p>
+            </motion.p>
           </div>
         </div>
     </section>
